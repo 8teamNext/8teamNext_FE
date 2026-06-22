551fe8bf9de4e86bc6aa945e25eeabf60bb9e251
haracedaily <nj7001@naver.com>
2026-06-22 17:40:07 +0900

    [fix] change currentPage

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ import { UserProfile, ResumeGithubResponse, api } from "./utils/api";
 
 export default function App() {
   // const [currentPage, setCurrentPage] = useState<string>("analysistest");
-  const [currentPage, setCurrentPage] = useState<string>("leancage-test");
+  // const [currentPage, setCurrentPage] = useState<string>("leancage-test");
+  const [currentPage, setCurrentPage] = useState<string>("home");
   const [user, setUser] = useState<UserProfile | null>(null);
   const [resumeGithubResult, setResumeGithubResult] = useState<ResumeGithubResponse | null>(null);
 
