c09191204085cc1510b6aeba883d51afcad36640
EunGyeoll <zytb77@gmail.com>
2026-06-24 16:38:50 +0900

    feat: 모의면접 ui 개선 및 로딩 성능 최적화
    - sample_answer lazy로딩
    - 면접관 스타일 선택가능
    - 꼬리질문 추가

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ import MyPage from "./pages/MyPage";
 import { UserProfile, ResumeGithubResponse, api } from "./utils/api";
 
 export default function App() {
-  // const [currentPage, setCurrentPage] = useState<string>("analysistest");
-  // const [currentPage, setCurrentPage] = useState<string>("leancage-test");
-  const [currentPage, setCurrentPage] = useState<string>("home");
+  const [currentPage, setCurrentPage] = useState<string>("analysistest"); //민정님
+  // const [currentPage, setCurrentPage] = useState<string>("leancage-test"); //팀장님
+  // const [currentPage, setCurrentPage] = useState<string>("home"); //채란님
   const [user, setUser] = useState<UserProfile | null>(null);
   const [resumeGithubResult, setResumeGithubResult] = useState<ResumeGithubResponse | null>(null);
 
